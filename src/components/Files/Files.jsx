057d26cb2c34cd8fba5fd94af46373ccb55a447d
Sengeer <sengeer@mail.ru>
2024-02-03 13:12:45 +0300

    feat: describe styles for Files

--- src/components/Files/Files.jsx
+++ src/components/Files/Files.jsx
@@ -5,7 +5,16 @@ function Files() {
   return (
     <div
       className="files">
-      <h2>Проводник</h2>
+      <h2
+        className="files__title">
+        Проводник
+      </h2>
+      <div className="files__container">
+        <p
+          className="files__message">
+            Пусто
+        </p>
+      </div>
     </div>
   );
 }
